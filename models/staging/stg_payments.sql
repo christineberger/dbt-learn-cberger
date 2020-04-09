89d1e9f445dbfd2ebfcb1e7011be4caccf0abff6
Christine <christinebergersd@gmail.com>
2020-04-09 14:17:55 -0500

    Added first exercise of dbtLearn

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,18 @@
+with 
+
+source as (
+    select * from {{ source('stripe', 'payment') }}
+),
+
+renamed as (
+    select
+        id as payment_id,
+        "orderID" as order_id,
+        "paymentMethod" as payment_method,
+        amount,
+        created,
+        _batched_at
+    from source
+)
+
+select * from renamed
